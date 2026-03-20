12f49e7eaef52c1481379c5d5cd335dcef3d2663
aagneye-syam <saagneye2003@gmail.com>
2026-03-20 23:14:52 +0530

    Create api.js with empty function skeletons

---
+++ src/utils/api.js
@@ -0,0 +1,11 @@
+export function geocodeCity(cityName) {
+  // TODO
+}
+
+export function fetchOSMPlaces(lat, lon, radius, osmTag, osmValue) {
+  // TODO
+}
+
+export function fetchOCMPlaces(lat, lon, radiusMeters) {
+  // TODO
+}
