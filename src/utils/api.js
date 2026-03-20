ed8bda530cdbefa6e43d1c5c9f6fb6d1322f1d63
aagneye-syam <saagneye2003@gmail.com>
2026-03-20 23:14:58 +0530

    Add fetch call to geocodeCity

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,6 @@
 export function geocodeCity(cityName) {
+  const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(cityName)}&format=json&limit=1`;
+  const response = fetch(url, { headers: { 'Accept-Language': 'en' } });
   // TODO
 }
 
