8f87469693d79d3622ced76d98abb258a3e4e84e
aagneye-syam <saagneye2003@gmail.com>
2026-03-20 23:16:09 +0530

    Add async/await and JSON parsing in geocodeCity

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,9 @@
 // API utilities for PlaceMapper Pro
 
-export function geocodeCity(cityName) {
+export async function geocodeCity(cityName) {
   const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(cityName)}&format=json&limit=1`;
-  const response = fetch(url, {
+  const response = await fetch(url, {
     headers: { 'Accept-Language': 'en' }
   });
+  const data = await response.json();
 }
