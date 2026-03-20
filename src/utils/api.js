a5e91eb28a7c79ad85c2c7b28805b5c0fb5d94bc
aagneye-syam <saagneye2003@gmail.com>
2026-03-20 23:16:03 +0530

    Add Accept-Language header to geocodeCity fetch

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,5 +2,7 @@
 
 export function geocodeCity(cityName) {
   const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(cityName)}&format=json&limit=1`;
-  const response = fetch(url);
+  const response = fetch(url, {
+    headers: { 'Accept-Language': 'en' }
+  });
 }
