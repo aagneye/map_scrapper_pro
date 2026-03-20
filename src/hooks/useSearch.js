d7c2a94da0394044d2ab3bd55eeb5d935b25da66
aagneye-syam <saagneye2003@gmail.com>
2026-03-20 23:29:58 +0530

    Add imports for utilities and constants

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,1 +1,4 @@
 import { useState } from 'react';
+import { geocodeCity, fetchOSMPlaces, fetchOCMPlaces } from '../utils/api.js';
+import { parseOSMResults, parseOCMResults } from '../utils/parser.js';
+import { CATEGORIES } from '../constants/index.js';
