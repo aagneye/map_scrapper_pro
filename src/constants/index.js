115bdda1542ee629199a1504eea61aef440d4acc
aagneye-syam <saagneye2003@gmail.com>
2026-03-20 23:13:16 +0530

    Add EV Charging Stations category

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,10 @@
-export const CATEGORIES = [];
+export const CATEGORIES = [
+  {
+    label: "⚡ EV Charging Stations",
+    osmTag: "amenity",
+    osmValue: "charging_station",
+    useOCM: true
+  }
+];
 
 export const RADIUS_OPTIONS = [];
