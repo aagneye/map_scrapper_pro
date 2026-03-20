1bab2a2de016b299d35718e1f9b3c3d63735c5df
aagneye-syam <saagneye2003@gmail.com>
2026-03-20 23:20:46 +0530

    Add headers array and data mapping to rows

--- src/utils/exporter.js
+++ src/utils/exporter.js
@@ -3,5 +3,25 @@
 import * as XLSX from 'xlsx';
 
 export function exportToExcel(data, cityName, categoryLabel) {
-  // TODO: implement
+  const headers = [
+    "#", "Name", "Address", "Latitude", "Longitude", "Phone", "Website",
+    "Rating", "Reviews", "Ports", "Connector Types", "Opening Hours", "Category", "Source"
+  ];
+
+  const rows = data.map((item, index) => [
+    index + 1,
+    item.name,
+    item.address,
+    item.lat,
+    item.lon,
+    item.phone,
+    item.website,
+    item.rating,
+    item.reviews,
+    item.ports,
+    item.connector_types,
+    item.opening_hours,
+    item.category,
+    item.source
+  ]);
 }
